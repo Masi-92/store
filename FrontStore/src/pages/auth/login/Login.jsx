d2c5b3bf413a4e21835b941180e250cd643e5cfb
Masi-92 <masoud.izadi@92gmali.com>
2024-02-29 23:29:25 +0100

    logisch

--- FrontStore/src/pages/auth/login/Login.jsx
+++ FrontStore/src/pages/auth/login/Login.jsx
@@ -1,9 +1,19 @@
 
+import { useState } from "react"
 import style from "./login.module.scss"
 
 const Login = () => {
+
+    const [email,setEmail]= useState("")
+    const [password,setPassword]= ("")
   return (
-    <div>Login</div>
+    <div>Login
+
+
+
+
+
+    </div>
   )
 }
 
