663b34446e17944fae00c4fcc3cc8c5d07edd926
Masi-92 <masoud.izadi@92gmali.com>
2024-02-29 23:26:02 +0100

    fron router

---
+++ FrontStore/src/pages/auth/login/Login.jsx
@@ -0,0 +1,10 @@
+
+import style from "./login.module.scss"
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
--- FrontStore/src/router/Router.jsx
+++ FrontStore/src/router/Router.jsx
@@ -1,14 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Register from "../pages/auth/register/Register";
+import Login from "../pages/auth/login/Login";
 
 const Router = () => {
   return (
     <Routes>
-   
-      <Route path="/register" element={<Register/>} />
- 
-   
-
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
