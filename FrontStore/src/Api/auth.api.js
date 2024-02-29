95f0992d9ac1e9d9ab797c8d33b8b4fe49ff0f6c
Masi-92 <masoud.izadi@92gmali.com>
2024-02-29 23:20:14 +0100

    1

--- FrontStore/src/Api/auth.api.js
+++ FrontStore/src/Api/auth.api.js
@@ -1,18 +1,9 @@
 import myApi from "./api";
 
+export const register = (body) => {
+  return myApi.post("/auth/register", body);
+};
 
-
-export const register =  (body) => {
-    return myApi.post("/auth/register",body)
-
-  };
-
-/*  export const login = (email,password)=>{
-return myApi.post("/auth/login",{email,password})
-
-  }
- */
-export const login =(email,password)=>{
-  return myApi.post("/auth/login",{email,password})
-}
-
+export const login = (email, password) => {
+  return myApi.post("/auth/login", { email, password });
+};
