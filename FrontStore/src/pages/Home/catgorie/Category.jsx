85be8a2a7ba3dfb8b6f58c36a597b6dd9c84815e
Masi-92 <masoud.izadi@92gmali.com>
2024-03-06 09:46:00 +0100

    min wid

--- FrontStore/src/pages/Home/catgorie/Category.jsx
+++ FrontStore/src/pages/Home/catgorie/Category.jsx
@@ -18,9 +18,9 @@ const Category = () => {
               className=" h-48 w-full object-cover border-solid rounded-md  transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none"
               alt={category.name}
             />
-            <button className="absolute bottom-3 left-1/2 rounded-md	border-b min-w-20 -translate-x-1/2 px-1 transition ease-in-out delay-150 bg-blue-100 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+            <p className="absolute bottom-3 left-1/2 text-center rounded-md	border-b min-w-20 -translate-x-1/2 px-1 transition ease-in-out delay-150 bg-blue-100 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
               {category.name}
-            </button>
+            </p>
           </div>
         ))}
       </div>
