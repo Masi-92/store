d9221e64bab228f142c26367783cc9187ca067c5
Masi-92 <masoud.izadi@92gmali.com>
2024-03-11 22:40:10 +0100

    admin panel

--- FrontStore/src/router/Router.jsx
+++ FrontStore/src/router/Router.jsx
@@ -3,6 +3,7 @@ import Register from "../pages/auth/register/Register";
 import Login from "../pages/auth/login/Login";
 import Home from "../pages/Home/Home";
 import Layout from "../component/layout/Layout";
+import AdminPanelLayout from "../component/admin/AdminPanelLayout";
 
 const Router = () => {
   return (
@@ -10,8 +11,10 @@ const Router = () => {
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/admin" element={<AdminPanelLayout/>}/>
       <Route path="/*" element={<Layout />}>
         <Route path="" element={<Home />} />
+
       </Route>
     </Routes>
   );
