6c6d58bd66d2651313c9d1ab02e0526bccf82806
Masi-92 <masoud.izadi@92gmali.com>
2024-03-15 08:09:28 +0100

    pro list

--- FrontStore/src/component/admin/ProductList.jsx
+++ FrontStore/src/component/admin/ProductList.jsx
@@ -4,9 +4,12 @@ import { Edit, Trash } from "iconsax-react";
 import { useEffect, useState } from "react";
 import { getAllProduct } from "../../Api/product";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@material-tailwind/react";
 
 const ProductList = () => {
   const [product, setProduct] = useState([]);
+  const navigate = useNavigate()
   useEffect(() => {
     getAllProduct()
       .then((res) => {
@@ -16,6 +19,10 @@ const ProductList = () => {
         toast("product not found");
       });
   }, []);
+
+  const handelClick = () => {
+    navigate("/admin/createProduct");
+  };
 
   const columns = [
     { header: "Name", accessorKey: "name" },
@@ -53,6 +60,14 @@ const ProductList = () => {
   return (
     <div>
       ProductList
+      <div className="flex justify-end">
+      <Button onClick={handelClick}
+      className="border-none  text-black bg-gradient-to-r from-purple-400 to-cyan-400 rounded-lg bg-cover bg-center font-bold text-lg h-12 w-48 mt-4 ml-4 transition duration-300 ease-in-out transform hover:scale-105 hover:bg-gradient-to-r hover:from-purple-900 hover:to-cyan-400 hover:bg-cover hover:bg-center"
+    >
+      Add Product
+    </Button>
+      </div>
+   
       <DataTable columns={columns} data={product} />
     </div>
   );
